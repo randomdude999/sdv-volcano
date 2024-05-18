af39292fca975f9dd675c6350d0f64e8e590c04f
trillian <trillian.git@r9.pm>
2024-05-18 14:52:15 +0300

    run a js formatter

--- index.ts
+++ index.ts
@@ -1,25 +1,18 @@
-import wasm_init, { main_update, GameSettings, render_map } from './pkg/sdv_volcano.js';
+import wasm_init, { GameSettings, main_update, render_map } from "./pkg/sdv_volcano.js";
 
 function get_settings() {
-    const seed_el = document.getElementById("seed") as HTMLInputElement;
-    const legacy_rng_el = document.getElementById("legacy_rng") as HTMLInputElement;
-    const has_caldera_el = document.getElementById("has_caldera") as HTMLInputElement;
-    const post_1_6_4_el = document.getElementById("post_1_6_4") as HTMLInputElement;
-    const cracked_coconut_el = document.getElementById("cracked_coconut") as HTMLInputElement;
-    const special_charm_el = document.getElementById("special_charm") as HTMLInputElement;
-    const max_luck_lvl_el = document.getElementById("max_luck_lvl") as HTMLInputElement;
-    const days_played_el = document.getElementById("days_played") as HTMLInputElement;
+    const get_el = (id: string) => document.getElementById(id) as HTMLInputElement;
     //document.getElementById("caldera_wrapper").classList.toggle("hidden", !post_1_6_4_el.checked);
     // ^ actually this just looks distracting
     const settings = new GameSettings();
-    settings.seed = +seed_el.value;
-    settings.legacy_rng = legacy_rng_el.checked;
-    settings.post_1_6_4 = post_1_6_4_el.checked;
-    settings.has_caldera = has_caldera_el.checked;
-    settings.cracked_golden_coconut = cracked_coconut_el.checked;
-    settings.max_luck_lvl = Math.max(+max_luck_lvl_el.value, 0);
-    settings.days_played = Math.max(+days_played_el.value, 1);
-    settings.special_charm = special_charm_el.checked;
+    settings.seed = +get_el("seed").value;
+    settings.legacy_rng = get_el("legacy_rng").checked;
+    settings.post_1_6_4 = get_el("post_1_6_4").checked;
+    settings.has_caldera = get_el("has_caldera").checked;
+    settings.cracked_golden_coconut = get_el("cracked_coconut").checked;
+    settings.max_luck_lvl = Math.max(+get_el("max_luck_lvl").value, 0);
+    settings.days_played = Math.max(+get_el("days_played").value, 1);
+    settings.special_charm = get_el("special_charm").checked;
     return settings;
 }
 
@@ -44,26 +37,27 @@ async function main() {
         map_canvas.classList.add("hidden");
         map_placeholder.classList.remove("hidden");
         map_notes.innerHTML = "";
-    }
+    };
 
     reset_canvas();
 
     const select_layout_handler = (lvl: number, layout: number) => {
-        const use_big = (document.getElementById("big_tiles") as HTMLInputElement).checked;
+        const use_big = (document.getElementById("big_tiles") as HTMLInputElement)
+            .checked;
         const settings = get_settings();
         const tile_size = use_big ? 16 : 8;
         map_canvas.width = map_canvas.height = 64 * tile_size;
         const tiles_img = use_big ? big_tiles : small_tiles;
-        const hints = render_map(settings, lvl, layout, map_ctx, tiles_img, tile_size);
+        const notes = render_map(settings, lvl, layout, map_ctx, tiles_img, tile_size);
         last_lvl = lvl;
         last_layout = layout;
         map_canvas.classList.remove("hidden");
         map_placeholder.classList.add("hidden");
-        map_notes.innerHTML = hints;
+        map_notes.innerHTML = notes;
     };
 
     document.getElementById("big_tiles").addEventListener("input", () => {
-        if(typeof last_lvl == 'number') {
+        if (typeof last_lvl == "number") {
             select_layout_handler(last_lvl, last_layout);
         }
     });
@@ -73,7 +67,7 @@ async function main() {
         const res = main_update(settings);
         document.getElementById("temp").innerText = res;
         reset_canvas();
-        for(const el of document.getElementsByClassName("layout-btn")) {
+        for (const el of document.getElementsByClassName("layout-btn")) {
             el.addEventListener("click", (ev) => {
                 const el = ev.target as HTMLElement;
                 const lvl = +el.getAttribute("data-lvl");
@@ -85,14 +79,12 @@ async function main() {
 
     update();
 
-    //const el = document.getElementById("b");
-    //el!.addEventListener("click", update);
-    for(const el of document.getElementsByClassName("setting")) {
+    for (const el of document.getElementsByClassName("setting")) {
         el.addEventListener("input", update);
     }
 
     document.getElementById("spam")?.addEventListener("click", () => {
-        for(let i = 0; i < 1000; i++) update();
+        for (let i = 0; i < 1000; i++) update();
     });
 }
 
